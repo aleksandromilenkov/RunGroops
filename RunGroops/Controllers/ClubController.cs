42825c570d1c636c19d4a354d6eeded695d1b37f
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-11-03 18:57:06 +0100

    creating Race Controller and Race View, Club Controller and Club View, and adding navigation to them.

--- RunGroops/Controllers/ClubController.cs
+++ RunGroops/Controllers/ClubController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroops.Data;
+using RunGroops.Models;
 
 namespace RunGroops.Controllers {
     public class ClubController : Controller {
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context) {
+            this._context = context;
+        }
         public IActionResult Index() {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id) {
+            Club club = _context.Clubs.FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }
--- RunGroops/Controllers/RaceController.cs
+++ RunGroops/Controllers/RaceController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroops.Data;
 
 namespace RunGroops.Controllers {
     public class RaceController : Controller {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context) {
+            this._context = context;
+        }
         public IActionResult Index() {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
