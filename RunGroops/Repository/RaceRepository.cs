3d25eb32b8fd1bc931dcedc71193af402c13af5e
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-11-04 17:15:17 +0100

    implementing interfaces and repositories for club and race

---
+++ RunGroops/Repository/RaceRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroops.Data;
+using RunGroops.Interfaces;
+using RunGroops.Models;
+
+namespace RunGroops.Repository {
+    public class RaceRepository : IRaceRepository {
+        private readonly ApplicationDbContext _context;
+        public RaceRepository(ApplicationDbContext context) {
+            _context = context;
+
+        }
+        public bool Add(Race race) {
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race club) {
+            _context.Remove(club);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll() {
+            return await _context.Races.ToListAsync();
+        }
+
+        public async Task<Race> GetByIdAsync(int id) {
+            return await _context.Races.FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<IEnumerable<Race>> GetRacesByCity(string city) {
+            return await _context.Races.Where(r => r.Address.City == city).ToListAsync();
+        }
+
+        public bool Save() {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Race club) {
+            throw new NotImplementedException();
+        }
+    }
+}
