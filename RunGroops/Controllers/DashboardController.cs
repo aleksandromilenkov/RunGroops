8bdd14d0191c00a128cf251bbdccdde06e954188
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-11-09 13:45:48 +0100

    limiting the view for unauthenticated users and adding dashboard and logout actions for the authenticated users

---
+++ RunGroops/Controllers/DashboardController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroops.Controllers {
+    public class DashboardController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
