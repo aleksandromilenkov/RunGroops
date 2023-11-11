eb51b6150a4084924c544d0674eef64671e961b9
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-11-11 17:18:20 +0100

    fetching Geo Location and displaying clubs in the home page based on your geo location

--- RunGroops/Controllers/HomeController.cs
+++ RunGroops/Controllers/HomeController.cs
@@ -1,17 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroops.Helpers;
+using RunGroops.Interfaces;
 using RunGroops.Models;
+using RunGroops.ViewModels;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace RunGroops.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository, IHttpContextAccessor httpContextAccessor) {
             _logger = logger;
+            this._clubRepository = clubRepository;
+            this._httpContextAccessor = httpContextAccessor;
         }
 
-        public IActionResult Index() {
-            return View();
+        public async Task<IActionResult> Index() {
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            bool isAuthenticated = _httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+            try {
+                string url = "https://ipinfo.io?token=db2790fdc3ef2d";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.Country = ipInfo.Country;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                homeViewModel.IsAuthenticated = isAuthenticated;
+                if (homeViewModel.City != null) {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception e) {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy() {
---
+++ RunGroops/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RunGroops.Models;
+
+namespace RunGroops.ViewModels {
+    public class HomeViewModel {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public bool IsAuthenticated { get; set; }
+    }
+}
